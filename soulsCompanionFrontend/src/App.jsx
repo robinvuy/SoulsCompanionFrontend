bd482ba1584d4fd0b8a518d56dbacae8f226be1d
Robin V <robinvuy@gmail.com>
2026-08-22 20:08:58 +0100

    connecting frontend to backend

--- soulsCompanionFrontend/src/App.jsx
+++ soulsCompanionFrontend/src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { MOCK_BOSSES } from './data/mockBosses';
 import './App.css'
-import Header from './components/Header';
+import Header from './components/Header'; 
 import BossCard from './components/BossCard';
 
 export default function App() {
-  const [bosses, setBosses] = useState(MOCK_BOSSES);
+  const [bosses, setBosses] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/bosses')
+        if (!response.ok) throw new Error('Failed to fetch')
+        const result = await response.json()
+        setBosses(result)
+      } catch (err) {
+        console.log(err)
+      } 
+    };
+
+    fetchData()
+  }, []);
 
   const toggleDefeated = (id) => {
     setBosses((prevBosses) =>
